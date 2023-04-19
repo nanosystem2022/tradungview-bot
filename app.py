0a18ffea6c4d3f57bd2066993b0be6b26b192c1f
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-04-19 18:12:49 +0330

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def webhook():
     return str(responses)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 443)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
