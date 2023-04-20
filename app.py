0fa8fe71b9be3837aeed9d0144c315db1cddccce
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-04-20 12:04:07 +0330

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,10 @@ def home():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.errorhandler(405)
+def page_not_found(e):
+    return render_template('405.html'), 405
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
