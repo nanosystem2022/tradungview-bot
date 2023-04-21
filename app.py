cb4683ff648afdbeb3e56562d4ee0c3b747c9dcb
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-04-22 02:49:20 +0330

    Update app.py

--- app.py
+++ app.py
@@ -92,6 +92,18 @@ def close_position(data):
             
         position_open = False
         print(f"Closed position on {exchange}: {symbol}")
+        
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run()
