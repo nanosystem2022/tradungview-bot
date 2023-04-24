c70d85660ac1833c23f8b1c98da5daf9e4a4705f
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-04-24 14:26:26 +0330

    Update app.py

--- app.py
+++ app.py
@@ -6,32 +6,61 @@ from flask import Flask, request
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
-USE_BINANCE_TESTNET = True  # Set to False to use the real Binance environment
+with open("config.json") as f:
+    config = json.load(f)
 
 binance_config = {
-    'apiKey': 'YOUR_BINANCE_API_KEY',
-    'secret': 'YOUR_BINANCE_SECRET_KEY',
+    'apiKey': config["EXCHANGES"]["binanceusdm"]["API_KEY"],
+    'secret': config["EXCHANGES"]["binanceusdm"]["API_SECRET"],
     'options': {
         'defaultType': 'future',  # Set the default type to 'future'
     },
 }
 
+USE_BINANCE_TESTNET = config["EXCHANGES"]["binanceusdm"]["TESTNET"]
+
 if USE_BINANCE_TESTNET:
     binance_config.update({
         'urls': {
             'api': {
-                'public': 'https://testnet.binancefuture.com',
-                'private': 'https://testnet.binancefuture.com',
+                'public': 'https://testnet.binancefuture.com/fapi/v1',
+                'private': 'https://testnet.binancefuture.com/fapi/v1',
+                'fapiPublic': 'https://testnet.binancefuture.com/fapi/v1',
+                'fapiPrivate': 'https://testnet.binancefuture.com/fapi/v1'
             }
         },
     })
 
 binance = ccxt.binance(binance_config)
 
-bybit = ccxt.bybit({
-    'apiKey': 'YOUR_BYBIT_API_KEY',
-    'secret': 'YOUR_BYBIT_SECRET_KEY',
-})
+bybit_config = {
+    'apiKey': config["EXCHANGES"]["BYBIT"]["API_KEY"],
+    'secret': config["EXCHANGES"]["BYBIT"]["API_SECRET"],
+}
+
+bybit = ccxt.bybit(bybit_config)
+
+def execute_order(exchange, order_type, symbol, percentage, price, side):
+    balance = exchange.fetch_balance({'type': 'future'})
+    base_currency = symbol.split('/')[0]
+    
+    if base_currency in balance['total']:
+        total_balance = balance['total'][base_currency]
+        quantity = total_balance * (percentage / 100)
+
+        if order_type == 'market':
+            if side == 'long':
+                exchange.create_market_buy_order(symbol, quantity)
+            elif side == 'short':
+                exchange.create_market_sell_order(symbol, quantity)
+        elif order_type == 'limit':
+            if side == 'long':
+                exchange.create_limit_buy_order(symbol, quantity, price)
+            elif side == 'short':
+                exchange.create_limit_sell_order(symbol, quantity, price)
+        logging.info(f"{exchange.name}: {side} {order_type} order executed for {symbol} with quantity {quantity} and price {price}")
+    else:
+        logging.error(f"{exchange.name}: Unable to find balance for {base_currency}")
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -42,41 +71,17 @@ def webhook():
         symbol = data['symbol']
         trade_side = data['side']
         trade_type = data['type']
-        quantity = data['quantity']
+        percentage = data['percentage']
         price = data['price']
 
-        if trade_type == 'market':
-            execute_market_order(binance, symbol, quantity, trade_side)
-            execute_market_order(bybit, symbol, quantity, trade_side)
-        elif trade_type == 'limit':
-            execute_limit_order(binance, symbol, quantity, price, trade_side)
-            execute_limit_order(bybit, symbol, quantity, price, trade_side)
+        execute_order(binance, trade_type, symbol, percentage, price, trade_side)
+        execute_order(bybit, trade_type, symbol, percentage, price, trade_side)
 
     except Exception as e:
         logging.error(f"Error processing webhook data: {e}")
         return "error", 500
 
     return "success", 200
-
-def execute_market_order(exchange, symbol, quantity, side):
-    try:
-        if side == 'long':
-            exchange.create_market_buy_order(symbol, quantity)
-        elif side == 'short':
-            exchange.create_market_sell_order(symbol, quantity)
-        logging.info(f"{exchange.name}: {side} market order executed for {symbol} with quantity {quantity}")
-    except Exception as e:
-        logging.error(f"{exchange.name}: Error executing {side} market order for {symbol}: {e}")
-
-def execute_limit_order(exchange, symbol, quantity, price, side):
-    try:
-        if side == 'long':
-            exchange.create_limit_buy_order(symbol, quantity, price)
-        elif side == 'short':
-            exchange.create_limit_sell_order(symbol, quantity, price)
-        logging.info(f"{exchange.name}: {side} limit order executed for {symbol} with quantity {quantity} and price {price}")
-    except Exception as e:
-        logging.error(f"{exchange.name}: Error executing {side} limit order for {symbol}: {e}")
 
 if __name__ == '__main__':
     app.run()
