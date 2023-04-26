8a33e292e51622b2a790e86e7eeca065d0c742ad
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-04-27 02:29:41 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,125 +1,67 @@
 import os
-import json
 import ccxt
-from flask import Flask, request, render_template
-from errors import errors_bp
+import json
+from flask import Flask, request
 
 app = Flask(__name__)
-app.register_blueprint(errors_bp)
 
 # خواندن تنظیمات از فایل config.json
 with open('config.json') as config_file:
     config = json.load(config_file)
 
-# ایجاد اتصال به صرافی‌ها
-exchanges = {}
-if config['EXCHANGES']['binanceusdm']['ENABLED']:
-    exchange_config = {
-        'apiKey': config['EXCHANGES']['binanceusdm']['API_KEY'],
-        'secret': config['EXCHANGES']['binanceusdm']['API_SECRET'],
-        'options': {'defaultType': 'future'},
-        'enableRateLimit': True
-    }
-    if config['EXCHANGES']['binanceusdm']['TESTNET']:
-        exchange_config['urls'] = {
-            'api': {
-                'public': 'https://testnet.binancefuture.com/fapi/v1',
-                'private': 'https://testnet.binancefuture.com/fapi/v1',
-                'fapiPublic': 'https://testnet.binancefuture.com/fapi/v1',
-                'fapiPrivate': 'https://testnet.binancefuture.com/fapi/v1'
-            },
-            'www': 'https://testnet.binance.vision',
-            'doc': 'https://binance-docs.github.io/apidocs/testnet/en/',
-            'test': {
-                'futures': 'https://testnet.binancefuture.com',
-                'margin': 'https://testnet.binance.vision',
-                'v3': 'https://testnet.binance.vision/api'
-            }
-        }
-    exchanges['binanceusdm'] = ccxt.binance(exchange_config)
+# تنظیمات API برای بایننس و بای‌بیت
+bybit_config = config["EXCHANGES"]["BYBIT"]
 
-if config['EXCHANGES']['BYBIT']['ENABLED']:
-    exchanges['bybit'] = ccxt.bybit({
-        'apiKey': config['EXCHANGES']['BYBIT']['API_KEY'],
-        'secret': config['EXCHANGES']['BYBIT']['API_SECRET'],
-        'enableRateLimit': True
+use_binance_futures = False
+if "BINANCE-FUTURES" in config["EXCHANGES"]:
+    if config["EXCHANGES"]["BINANCE-FUTURES"]["ENABLED"]:
+        print("Binance is enabled!")
+        use_binance_futures = True
+
+        binance = ccxt.binance({
+            "apiKey": config["EXCHANGES"]["BINANCE-FUTURES"]["API_KEY"],
+            "secret": config["EXCHANGES"]["BINANCE-FUTURES"]["API_SECRET"],
+            "options": {"defaultType": "future"},
+            "urls": {
+                "api": {
+                    "public": "https://testnet.binancefuture.com/fapi/v1",
+                    "private": "https://testnet.binancefuture.com/fapi/v1",
+                },
+            },
+        })
+        binance.set_sandbox_mode(True)
+
+if bybit_config["ENABLED"]:
+    bybit = ccxt.bybit({
+        "apiKey": bybit_config["API_KEY"],
+        "secret": bybit_config["API_SECRET"],
+        "enableRateLimit": True,
     })
 
-open_position = False
+def create_order(exchange, symbol, side, amount, price=None):
+    if price:
+        return exchange.create_order(symbol, "limit", side, amount, price)
+    else:
+        return exchange.create_order(symbol, "market", side, amount)
 
-# ... بقیه کد بدون تغییر
-
-
-@app.route('/webhook', methods=['POST'])
+@app.route("/webhook", methods=["POST"])
 def webhook():
-    data = request.json
-    print("دریافت سیگنال:", data)
+    data = json.loads(request.data)
 
-    exchange = data['signal'].lower()  # Use 'signal' key instead of 'exchange'
-    if exchange in exchanges:
-        execute_trade(exchanges[exchange], data)
+    exchange_name = data["exchange"]
+    symbol = data["symbol"]
+    side = data["side"]
+    amount = data["amount"]
+    price = data.get("price", None)
+
+    if exchange_name == "BINANCE-FUTURES" and use_binance_futures:
+        order = create_order(binance, symbol, side, amount, price)
+    elif exchange_name == "BYBIT" and bybit_config["ENABLED"]:
+        order = create_order(bybit, symbol, side, amount, price)
     else:
-        print(f"صرافی {exchange} فعال نیست.")
+        return "Invalid exchange name or exchange is not enabled", 400
 
-    return {
-        'status': 'success'
-    }, 200
+    return json.dumps(order), 200
 
-def execute_trade(exchange, data):
-    global open_position
-
-    symbol = data['symbol']
-    side = data['side']
-    if 'percentage' in data:
-        percentage = float(data['percentage'])
-    price = float(data['price'])
-
-    if side == 'closelong' or side == 'closeshort':
-        if open_position:
-            close_position(exchange, symbol, side)
-            open_position = False
-        else:
-            print("هیچ معامله‌ای برای بستن وجود ندارد.")
-    else:
-        if not open_position:
-            if 'percentage' in data:
-                # دریافت موجودی کاربر
-                balance = exchange.fetch_balance()
-
-                # محاسبه مقدار معامله بر اساس درصد موجودی
-                base_currency = symbol.split('/')[0]
-                quote_currency = symbol.split('/')[1]
-                available_balance = balance[quote_currency]['free']
-                amount = (percentage / 100) * available_balance / price
-
-            # اجرای معامله
-            if side == 'long':
-                # خرید (long) در حالت فیوچرز
-                exchange.create_market_buy_order(symbol, amount)
-                open_position = True
-            elif side == 'short':
-                # فروش (short) در حالت فیوچرز
-                exchange.create_market_sell_order(symbol, amount)
-                open_position = True
-            else:
-                print("نوع معامله نامعتبر است.")
-        else:
-            print("یک معامله باز است. لطفاً ابتدا معامله فعلی را ببندید.")
-
-def close_position(exchange, symbol, side):
-    if side == 'closelong':
-        position_side = 'long'
-    else:
-        position_side = 'short'
-
-    if exchange == exchanges['binanceusdm']:
-        exchange.private_post_position_close({
-            'symbol': exchange.market_id(symbol),
-            'positionSide': position_side,
-            'reduceOnly': True
-        })
-    else:
-        print("بستن معامله در این صرافی پشتیبانی نمی‌شود.")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
